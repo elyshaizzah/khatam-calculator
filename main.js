78e271a3e275d9dcc80c5b57d1645bb9fb9f9809
Elysha Izzah <strawberryelysha2005@gmail.com>
2019-05-25 16:07:01 +0800

    did by days and

---
+++ main.js
@@ -0,0 +1,15 @@
+function calculateDaysNeeded(){
+    const totalpage = 604; //totalpage
+    const input = document.getElementById("number").value
+    const value = totalpage/input    
+ document.getElementById("results").innerHTML = value
+
+}
+
+function calculateDaysNeeded(){
+    const totalpage = 604; //totalpage
+    const input = document.getElementById("Number").value
+    const value = totalpage/input    
+ document.getElementById("Results").innerHTML = value
+
+}
